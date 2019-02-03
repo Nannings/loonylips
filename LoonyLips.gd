0b6fc8260773cc351e2bf98ee8ba025a04cd6094
Nannings <mrnannings@gmail.com>
2019-02-03 19:07:49 +0100

    5 3

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,4 +3,5 @@ extends Node2D
 func _ready():
 	var prompt = ["Yann", "pizza", "feeling", "feeling", "feeling"]
 	var story = "Once upon a time a %s ate a %s  and felt very %s. %s %s"
-	print(story % prompt)
+#	$Blackboard/StoryText.text = (story % prompt)
+	$Blackboard/StoryText.bbcode_text = (story % prompt)
