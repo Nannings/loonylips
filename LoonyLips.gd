c07a5f34d6ee619b4060ea7ec2e0835f74fa6d8c
Nannings <mrnannings@gmail.com>
2019-02-04 11:40:57 +0100

    6 4

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -3,5 +3,14 @@ extends Node2D
 func _ready():
 	var prompt = ["Yann", "pizza", "feeling", "feeling", "feeling"]
 	var story = "Once upon a time a %s ate a %s  and felt very %s. %s %s"
-#	$Blackboard/StoryText.text = (story % prompt)
-	$Blackboard/StoryText.bbcode_text = (story % prompt)
+	$Blackboard/StoryText.text = (story % prompt)
+#	$Blackboard/StoryText.bbcode_text = (story % prompt)
+	$Blackboard/TextBox.text = ""
+
+func _on_TextureButton_pressed():
+	var text = $Blackboard/TextBox.text
+	_on_TextBox_text_entered(text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.bbcode_text = new_text
+	$Blackboard/TextBox.text = ""
